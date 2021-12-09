33094aeb8a83bcf68c7720d2f09d79de04661224
Derpky <a@dierking.me>
2021-12-09 02:24:28 -0800

    tests: Improve the rebuild_all_music test

--- unplug-test/tests/rebuild_all_music.rs
+++ unplug-test/tests/rebuild_all_music.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 use log::info;
 use std::io::Cursor;
 use unplug::audio::transport::HpsStream;
+use unplug::audio::ReadSamples;
 use unplug::common::WriteTo;
 use unplug::data::music::MUSIC;
 use unplug::dvd::OpenFile;
@@ -18,11 +19,27 @@ fn test_rebuild_all_music() -> Result<()> {
         let mut reader = iso.open_file_at(&path)?;
         let mut original_bytes = vec![];
         reader.read_to_end(&mut original_bytes)?;
-
         let hps = HpsStream::open(&mut Cursor::new(&original_bytes), path)?;
+
         info!("Rebuilding HPS stream");
+        let mut rebuilt = match hps.channels.len() {
+            1 => HpsStream::from_adpcm_mono(&mut hps.reader(0).cast())?,
+            2 => {
+                HpsStream::from_adpcm_stereo(&mut hps.reader(0).cast(), &mut hps.reader(1).cast())?
+            }
+            other => panic!("unexpected channel count: {}", other),
+        };
+
+        // HACK: We currently lose the loop settings
+        rebuilt.loop_start = hps.loop_start;
+        // HACK: It seems like our end_address computations don't always match, but it isn't clear
+        // how to calculate it to match (or if it even matters...)
+        for (a, b) in hps.channels.iter().zip(rebuilt.channels.iter_mut()) {
+            b.address.end_address = a.address.end_address;
+        }
+
         let mut cursor = Cursor::new(vec![]);
-        hps.write_to(&mut cursor)?;
+        rebuilt.write_to(&mut cursor)?;
 
         let rebuilt_bytes = cursor.into_inner();
         assert!(original_bytes == rebuilt_bytes);
