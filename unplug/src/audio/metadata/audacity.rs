6ab0a58a7827fadc022934a679c054492e32f762
Derpky <a@dierking.me>
2021-12-15 18:50:22 -0800

    audio: Audacity label track reading

--- unplug/src/audio/metadata/audacity.rs
+++ unplug/src/audio/metadata/audacity.rs
@@ -4,7 +4,7 @@
 use crate::audio::cue::{self, Cue, CueKind};
 use crate::audio::Result;
 use std::borrow::Cow;
-use std::io::Write;
+use std::io::{BufRead, Write};
 
 /// Writes a set of cues to `writer` in Audacity's label track format. `sample_rate` is the sample
 /// rate of the source audio stream, needed to calculate time points for cues.
@@ -35,12 +35,56 @@ fn write_labels_impl(
     Ok(())
 }
 
+/// Reads cues from Audacity label track data in `reader`. `sample_rate` is the sample rate of the
+/// source audio stream, needed to calculate sample numbers for cues.
+pub fn read_labels(mut reader: impl BufRead, sample_rate: u32) -> Result<Vec<Cue>> {
+    read_labels_impl(&mut reader, sample_rate)
+}
+
+fn read_labels_impl(reader: &mut dyn BufRead, sample_rate: u32) -> Result<Vec<Cue>> {
+    let mut cues = vec![];
+    loop {
+        let mut line = String::new();
+        let len = reader.read_line(&mut line)?;
+        if len == 0 {
+            break;
+        }
+
+        // <start> [<end>] <name>
+        let mut tokens = line.trim_end_matches("\r\n").split('\t');
+        let start = match tokens.next().map(|t| t.parse::<f64>()) {
+            Some(Ok(start)) if start.is_finite() => start,
+            _ => continue, // Skip bad lines (e.g. lines starting with `/`)
+        };
+        // If the next token is a number, it is the end point and the next token is the name,
+        // otherwise the cue is a single point and this token is the name
+        let token = tokens.next().unwrap_or_default();
+        let (end, name) = match token.parse::<f64>() {
+            Ok(end) if end.is_finite() => (end, tokens.next().unwrap_or_default()),
+            _ => (start, token),
+        };
+
+        let start_sample = (start * f64::from(sample_rate)).round() as u64;
+        let end_sample = (end * f64::from(sample_rate)).round() as u64;
+        let cue = if cue::has_loop_prefix(name) {
+            Cue::new_loop(name, start_sample)
+        } else if end > start {
+            Cue::new_range(name, start_sample, end_sample - start_sample)
+        } else {
+            Cue::new(name, start_sample)
+        };
+        cues.push(cue);
+    }
+    Ok(cues)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io::Cursor;
 
     #[test]
-    fn test_write_labels() -> Result<()> {
+    fn test_write_labels() {
         let cues = vec![
             Cue::new("one", 0),
             Cue::new("two", 22050),
@@ -48,7 +92,7 @@ mod tests {
             Cue::new_loop("four", 88200),
         ];
         let mut labels = vec![];
-        write_labels(&mut labels, cues, 44100)?;
+        write_labels(&mut labels, cues, 44100).unwrap();
         let labels = String::from_utf8(labels).unwrap();
         assert_eq!(
             labels,
@@ -59,6 +103,26 @@ mod tests {
                 "2.000000\t2.000000\tloop:four\r\n",
             )
         );
-        Ok(())
+    }
+
+    #[test]
+    fn test_read_labels() {
+        let labels = Cursor::new(concat!(
+            "0.000000\t0.000000\tone\r\n",
+            "0.500000\ttwo\r\n",
+            "1.000000\t1.500000\tthree\r\n",
+            "\\\t6928.456055\t6928.456055\r\n",
+            "2.000000\t2.000000\tloop:four\r\n",
+        ));
+        let cues = read_labels(labels, 44100).unwrap();
+        assert_eq!(
+            cues,
+            &[
+                Cue::new("one", 0),
+                Cue::new("two", 22050),
+                Cue::new_range("three", 44100, 22050),
+                Cue::new_loop("loop:four", 88200),
+            ]
+        );
     }
 }
