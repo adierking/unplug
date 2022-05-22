7657ab95dd97629851df4ed1263a4d7f8532050f
Derpky <a@dierking.me>
2022-05-22 14:14:45 -0700

    tests: Reduce the number of .iso copies needed

--- unplug-test/tests/edit_disc_file.rs
+++ unplug-test/tests/edit_disc_file.rs
@@ -1,56 +1,43 @@
 use anyhow::Result;
 use log::info;
-use serial_test::serial;
 use std::cmp;
 use std::fs::{File, OpenOptions};
 use std::io::{Cursor, Read, SeekFrom};
+use std::path::Path;
 use unplug::dvd::{DiscStream, EditFile, OpenFile};
 use unplug_test as common;
 
+const OLD_QP_OFFSET: u32 = 0x3dd6e348;
+const NEW_QP_OFFSET: u32 = 0x280000;
+
+const OLD_QP_SIZE: u32 = 0x46b288b;
+
 #[test]
-#[serial]
-fn test_move_disc_file() -> Result<()> {
+fn test_edit_disc_file() -> Result<()> {
     common::init_logging();
-
     let copy_path = common::copy_iso()?;
-    info!("Opening {}", copy_path.to_str().unwrap());
-    let file = OpenOptions::new().read(true).write(true).open(&copy_path)?;
-    let mut iso = DiscStream::open(file)?;
-    common::check_iso(&iso)?;
 
-    info!("Moving qp.bin");
-    let qp = iso.files.at("qp.bin")?;
-    assert_eq!(iso.files.file(qp)?.offset, 0x3dd6e348);
-    iso.move_file(qp, 0x280000)?;
-    assert_eq!(iso.files.file(qp)?.offset, 0x280000);
-    drop(iso);
+    check_resize_qp(&copy_path, OLD_QP_SIZE + 1, OLD_QP_OFFSET)?; // Larger (no move)
+    check_resize_qp(&copy_path, OLD_QP_SIZE + 2, NEW_QP_OFFSET)?; // Larger (move)
+    check_resize_qp(&copy_path, OLD_QP_SIZE, NEW_QP_OFFSET)?; // Smaller (no move)
 
-    info!("Comparing ISOs");
-    let mut old_iso = common::open_iso()?;
-    let mut new_iso = DiscStream::open(File::open(&copy_path)?)?;
-    let old_qp = old_iso.files.file_at("qp.bin")?;
-    let new_qp = new_iso.files.file_at("qp.bin")?;
-    assert_eq!(old_qp.offset, 0x3dd6e348);
-    assert_eq!(new_qp.offset, 0x280000);
-    let old_reader = old_iso.open_file_at("qp.bin")?;
-    let new_reader = new_iso.open_file_at("qp.bin")?;
-    assert!(common::compare_streams(old_reader, new_reader)?);
+    check_move_file(&copy_path, 0x10000000)?;
+    check_move_file(&copy_path, OLD_QP_OFFSET)?;
+
+    check_edit_file(&copy_path)?;
+    check_replace_file(&copy_path)?;
+
     Ok(())
 }
 
-fn do_resize_qp_test(new_size: u32, expected_offset: u32) -> Result<()> {
-    common::init_logging();
-
-    let copy_path = common::copy_iso()?;
-    info!("Opening {}", copy_path.to_str().unwrap());
+fn check_resize_qp(copy_path: &Path, new_size: u32, expected_offset: u32) -> Result<()> {
+    info!("Opening {}", copy_path.display());
     let file = OpenOptions::new().read(true).write(true).open(&copy_path)?;
     let mut iso = DiscStream::open(file)?;
     common::check_iso(&iso)?;
 
     info!("Resizing qp.bin");
     let qp = iso.files.at("qp.bin")?;
-    assert_eq!(iso.files.file(qp)?.offset, 0x3dd6e348);
-    assert_eq!(iso.files.file(qp)?.size, 0x46b288b);
     iso.resize_file(qp, new_size)?;
     assert_eq!(iso.files.file(qp)?.offset, expected_offset);
     assert_eq!(iso.files.file(qp)?.size, new_size);
@@ -61,7 +48,7 @@ fn do_resize_qp_test(new_size: u32, expected_offset: u32) -> Result<()> {
     let mut new_iso = DiscStream::open(File::open(&copy_path)?)?;
     let old_qp = old_iso.files.file_at("qp.bin")?;
     let new_qp = new_iso.files.file_at("qp.bin")?;
-    assert_eq!(old_qp.offset, 0x3dd6e348);
+    assert_eq!(old_qp.offset, OLD_QP_OFFSET);
     assert_eq!(new_qp.offset, expected_offset);
     assert_eq!(new_qp.size, new_size);
 
@@ -72,31 +59,33 @@ fn do_resize_qp_test(new_size: u32, expected_offset: u32) -> Result<()> {
     Ok(())
 }
 
-#[test]
-#[serial]
-fn test_resize_disc_file_smaller() -> Result<()> {
-    do_resize_qp_test(0x46b288a, 0x3dd6e348)
+fn check_move_file(copy_path: &Path, new_offset: u32) -> Result<()> {
+    info!("Opening {}", copy_path.display());
+    let file = OpenOptions::new().read(true).write(true).open(&copy_path)?;
+    let mut iso = DiscStream::open(file)?;
+    common::check_iso(&iso)?;
+
+    info!("Moving qp.bin");
+    let qp = iso.files.at("qp.bin")?;
+    iso.move_file(qp, new_offset)?;
+    assert_eq!(iso.files.file(qp)?.offset, new_offset);
+    drop(iso);
+
+    info!("Comparing ISOs");
+    let mut old_iso = common::open_iso()?;
+    let mut new_iso = DiscStream::open(File::open(&copy_path)?)?;
+    let old_qp = old_iso.files.file_at("qp.bin")?;
+    let new_qp = new_iso.files.file_at("qp.bin")?;
+    assert_eq!(old_qp.offset, OLD_QP_OFFSET);
+    assert_eq!(new_qp.offset, new_offset);
+    let old_reader = old_iso.open_file_at("qp.bin")?;
+    let new_reader = new_iso.open_file_at("qp.bin")?;
+    assert!(common::compare_streams(old_reader, new_reader)?);
+    Ok(())
 }
 
-#[test]
-#[serial]
-fn test_resize_disc_file_larger_no_move() -> Result<()> {
-    do_resize_qp_test(0x46b288c, 0x3dd6e348)
-}
-
-#[test]
-#[serial]
-fn test_resize_disc_file_larger_move() -> Result<()> {
-    do_resize_qp_test(0x46b288d, 0x280000)
-}
-
-#[test]
-#[serial]
-fn test_edit_disc_file() -> Result<()> {
-    common::init_logging();
-
-    let copy_path = common::copy_iso()?;
-    info!("Opening {}", copy_path.to_str().unwrap());
+fn check_edit_file(copy_path: &Path) -> Result<()> {
+    info!("Opening {}", copy_path.display());
     let file = OpenOptions::new().read(true).write(true).open(&copy_path)?;
     let mut iso = DiscStream::open(file)?;
     common::check_iso(&iso)?;
@@ -104,19 +93,14 @@ fn test_edit_disc_file() -> Result<()> {
     info!("Editing qp.bin");
     let qp = iso.files.at("qp.bin")?;
     let mut stream = iso.edit_file(qp)?;
-    assert_eq!(stream.seek(SeekFrom::End(0))?, 0x46b288b);
+    assert_eq!(stream.seek(SeekFrom::End(0))?, OLD_QP_SIZE.into());
     assert_eq!(stream.write(&[0xaa, 0xbb])?, 1);
     assert_eq!(stream.write(&[0xaa, 0xbb])?, 0);
     Ok(())
 }
 
-#[test]
-#[serial]
-fn test_replace_disc_file() -> Result<()> {
-    common::init_logging();
-
-    let copy_path = common::copy_iso()?;
-    info!("Opening {}", copy_path.to_str().unwrap());
+fn check_replace_file(copy_path: &Path) -> Result<()> {
+    info!("Opening {}", copy_path.display());
     let file = OpenOptions::new().read(true).write(true).open(&copy_path)?;
     let mut iso = DiscStream::open(file)?;
     common::check_iso(&iso)?;
